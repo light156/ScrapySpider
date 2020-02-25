6dc2f001919c4b7a9f4d989a05487c1d3e25a650
wy <596205633@qq.com>
2020-02-25 19:43:49 +0800

    Firm

---
+++ main.py
@@ -0,0 +1,10 @@
+import sys
+import os
+
+from scrapy.cmdline import execute
+
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
+#execute(["scrapy", "crawl", "Claim"])
+#execute(["scrapy", "crawl", "Legacy"])
+execute(["scrapy", "crawl", "Firm"])
